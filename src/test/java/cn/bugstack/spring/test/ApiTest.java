bce7c665275ed1a3b73f20e48c66afd5f5837657
小傅哥 <184172133@qq.com>
2021-05-15 14:33:34 +0800

    小傅哥 small-spring-step-01

---
+++ src/test/java/cn/bugstack/spring/test/ApiTest.java
@@ -0,0 +1,29 @@
+package cn.bugstack.spring.test;
+
+import cn.bugstack.spring.BeanDefinition;
+import cn.bugstack.spring.BeanFactory;
+import cn.bugstack.spring.test.bean.UserService;
+import org.junit.Test;
+
+/**
+ * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ * 公众号：bugstack虫洞栈
+ * Create by 小傅哥(fustack)
+ */
+public class ApiTest {
+
+    @Test
+    public void test_BeanFactory(){
+        // 1.初始化 BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        // 2.注入bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+
+}
